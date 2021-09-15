c15661ecac8ba2e9f0aed8d383cd50454495cc3e
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-09-14 23:10:44 -0300

    test: adiciona teste de criação de funcionário com cpf duplicado

--- src/modules/employees/useCases/CreateEmployee.spec.ts
+++ src/modules/employees/useCases/CreateEmployee.spec.ts
@@ -2,6 +2,7 @@ import { Role } from '../domain/Role'
 import { IEmployeesRepository } from '../infra/repository/IEmployeesRepository'
 import { InMemoryEmployeesRepository } from '../infra/repository/in-memory/InMemoryEmployeesRepository'
 import { CreateEmployee } from './CreateEmployee'
+import { CpfAlreadyExistsError } from './error/CpfAlreadyExistsError'
 
 let employeesRepository: IEmployeesRepository
 let createEmployee: CreateEmployee
@@ -12,14 +13,32 @@ describe('Create Employee', () => {
     createEmployee = new CreateEmployee(employeesRepository)
   })
 
-  test('deve criar um usuário', async () => {
+  test('deve criar um funcionário', async () => {
     const employee = await createEmployee.execute({
-      cpf: '111.111.111-11',
-      name: 'Gabriel Mesquita',
-      password: '123',
+      cpf: 'valid-cpf',
+      name: 'valid-name',
+      password: 'valid-password',
       role: Role.ADMIN,
     })
 
     expect(employee).toHaveProperty('id')
+  })
+
+  test('não deve criar um funcionário com cpf duplicado', async () => {
+    await createEmployee.execute({
+      cpf: 'duplicated-cpf',
+      name: 'valid-name',
+      password: 'valid-password',
+      role: Role.ADMIN,
+    })
+
+    await expect(
+      createEmployee.execute({
+        cpf: 'duplicated-cpf',
+        name: 'Gabriel Mesquita',
+        password: '123',
+        role: Role.ADMIN,
+      })
+    ).rejects.toEqual(new CpfAlreadyExistsError('duplicated-cpf'))
   })
 })
