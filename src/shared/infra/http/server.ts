4396cd7dd2216d81e50958d4a649d0aaa3453fdc
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-09-16 22:25:18 -0300

    refactor: ativa regra eslint no-console

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import { app } from './app'
 
 app.listen(process.env.APP_PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on http://localhost:${process.env.APP_PORT}`)
 })
