63ecb5e96adc16f6679fc65a4bd462236121c3c2
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-09-18 21:14:57 -0300

    refactor: separa criação do controller

---
+++ src/shared/infra/http/factories/CreateEmployeeControllerFactory.ts
@@ -0,0 +1,13 @@
+import { PrismaEmployeesRepository } from '@modules/employees/infra/repository/prisma/PrismaEmployeesRepository'
+import { CreateEmployee } from '@modules/employees/useCases/CreateEmployee'
+import { CreateEmployeeController } from '@modules/employees/useCases/CreateEmployeeController'
+
+import { Controller } from '../protocols/Controller'
+
+export function makeCreateEmployeeController(): Controller {
+  const prismaEmployeesRepository = new PrismaEmployeesRepository()
+  const createEmployee = new CreateEmployee(prismaEmployeesRepository)
+  const createEmployeeController = new CreateEmployeeController(createEmployee)
+
+  return createEmployeeController
+}
--- src/shared/infra/http/routes/employee.routes.ts
+++ src/shared/infra/http/routes/employee.routes.ts
@@ -2,14 +2,8 @@ import { Router } from 'express'
 
 import { adaptRoute } from '@shared/infra/http/protocols/AdaptRoute'
 
-import { PrismaEmployeesRepository } from '@modules/employees/infra/repository/prisma/PrismaEmployeesRepository'
-import { CreateEmployee } from '@modules/employees/useCases/CreateEmployee'
-import { CreateEmployeeController } from '@modules/employees/useCases/CreateEmployeeController'
-
-const employeeRepository = new PrismaEmployeesRepository()
-const createEmployee = new CreateEmployee(employeeRepository)
-const createEmployeeController = new CreateEmployeeController(createEmployee)
+import { makeCreateEmployeeController } from '../factories/CreateEmployeeControllerFactory'
 
 export default (router: Router): void => {
-  router.post('/employees', adaptRoute(createEmployeeController))
+  router.post('/employees', adaptRoute(makeCreateEmployeeController()))
 }
