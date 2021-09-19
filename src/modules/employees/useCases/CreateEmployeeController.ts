ef96ed5c61dca9a04e77d2ad0461fc4006cf7e17
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-09-19 00:19:01 -0300

    feat: adiciona createEmployeeControllerValidator

--- src/modules/employees/useCases/CreateEmployeeController.ts
+++ src/modules/employees/useCases/CreateEmployeeController.ts
@@ -1,18 +1,20 @@
+import { ValidationError } from '@shared/errors/ValidationError'
 import { Controller } from '@shared/infra/http/protocols/Controller'
 import { HttpRequest } from '@shared/infra/http/protocols/HttpRequest'
-import {
-  badRequest,
-  created,
-  HttpResponse,
-  serverError,
-} from '@shared/infra/http/protocols/HttpResponse'
+import { badRequest, created, HttpResponse } from '@shared/infra/http/protocols/HttpResponse'
 
 import { CreateEmployee } from './CreateEmployee'
-
+import { schema } from './validations/CreateEmployeeControllerValidator'
 export class CreateEmployeeController implements Controller {
   constructor(private createEmployee: CreateEmployee) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { error } = schema.validate(httpRequest)
+
+    if (error) {
+      return badRequest(new ValidationError(error.details))
+    }
+
     const { cpf, name, password, role } = httpRequest.body
 
     const result = await this.createEmployee.execute({
---
+++ src/modules/employees/useCases/validations/CreateEmployeeControllerValidator.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi'
+
+import { Role } from '@modules/employees/domain/Role'
+
+export const schema = Joi.object({
+  body: Joi.object({
+    cpf: Joi.string().required().trim(),
+    name: Joi.string().required().trim(),
+    password: Joi.string().required().trim(),
+    password_confirmation: Joi.ref('password'),
+    role: Joi.string().valid(...Object.values(Role)),
+  }).with('password', 'password_confirmation'),
+})
