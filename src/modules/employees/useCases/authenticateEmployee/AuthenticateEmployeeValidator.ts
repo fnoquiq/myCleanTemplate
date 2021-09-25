d396934e10871748703c1c0cd0b6a41cae82ea0b
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-09-25 17:34:38 -0300

    refactor: altera Joi para @Hapi/Joi

--- src/modules/employees/useCases/authenticateEmployee/AuthenticateEmployeeValidator.ts
+++ src/modules/employees/useCases/authenticateEmployee/AuthenticateEmployeeValidator.ts
@@ -1,8 +1,8 @@
-import Joi from 'joi'
+import Joi from '@hapi/joi'
 
 export const authenticateEmployeeValidator = Joi.object({
   body: Joi.object({
-    cpf: Joi.string().required().trim(),
+    cpf: Joi.string().required(),
     password: Joi.string().required().trim(),
   }),
 })
--- src/modules/employees/useCases/createEmployee/CreateEmployeeValidator.ts
+++ src/modules/employees/useCases/createEmployee/CreateEmployeeValidator.ts
@@ -1,4 +1,4 @@
-import Joi from 'joi'
+import Joi from '@hapi/joi'
 
 import { Role } from '@modules/employees/domain/Role'
 
