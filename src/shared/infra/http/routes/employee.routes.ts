fc3ec24044e1c9d23f04378acb30e0cf84564646
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-09-26 14:14:15 -0300

    refactor: move factories pra dentro do useCase

--- src/shared/infra/http/routes/employee.routes.ts
+++ src/shared/infra/http/routes/employee.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import { adaptRoute } from '@shared/protocols/infra/AdaptRoute'
 
-import { makeCreateEmployeeController } from '@modules/employees/useCases/factories/CreateEmployeeFactory'
+import { makeCreateEmployeeController } from '@modules/employees/useCases/createEmployee/CreateEmployeeFactory'
 
 export default (router: Router): void => {
   router.post('/employees', adaptRoute(makeCreateEmployeeController()))
--- src/shared/infra/http/routes/login.routes.ts
+++ src/shared/infra/http/routes/login.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import { adaptRoute } from '@shared/protocols/infra/AdaptRoute'
 
-import { makeAuthenticateEmployeeController } from '@modules/employees/useCases/factories/AuthenticateEmployeeFactory'
+import { makeAuthenticateEmployeeController } from '@modules/employees/useCases/authenticateEmployee/AuthenticateEmployeeFactory'
 
 export default (router: Router): void => {
   router.post('/login', adaptRoute(makeAuthenticateEmployeeController()))
